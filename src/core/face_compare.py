5184e52d2bdc485b18e09d994017e68a02a50c65
EXP <289065406@qq.com>
2022-11-06 14:34:46 +0800

    代码优化

--- src/core/face_compare.py
+++ src/core/face_compare.py
@@ -11,18 +11,7 @@ from src.config import SETTINGS
 
 class FaceCompare :
 
-    def __init__(self, 
-        model_selection=0, static_image_mode=False, 
-        min_detection_confidence=0.5, min_tracking_confidence=0.5
-    ) -> None:
-        '''
-        构造函数
-        [params] args: main 入参
-        [params] model_selection: 距离模型:  0:短距离模式，适用于 2 米内的人脸; 1:全距离模型，适用于 5 米内的人脸
-        [params] static_image_mode: 人脸识别场景:  True:静态图片; False:视频流
-        [params] min_detection_confidence: 人脸检测模型的最小置信度值
-        [params] min_tracking_confidence: 跟踪模型的最小置信度值（仅视频流有效）
-        '''
+    def __init__(self) :
         pass
 
 
